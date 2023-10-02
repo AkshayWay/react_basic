d0f2e244380bf8cdce7045a0575d9aba1877066a
AkshayWay <38669737+AkshayWay@users.noreply.github.com>
2023-10-03 00:13:35 +0530

    useMemo hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Parent from "./Parent";
 import UseEffectPractice from "./useEffectComp/UseEffectPractice";
 import UseStatePractice from "./useStateComp/useStatePractice";
+import UseMemoPractice from "./useMemoComp/useMemoPractice";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/useEffect" element={<UseEffectPractice />} />
         <Route path="/useState" element={<UseStatePractice />} />
+        <Route path="/useMemo" element={<UseMemoPractice />} />
       </Routes>
     </Router>
   );
